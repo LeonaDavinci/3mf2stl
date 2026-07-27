4d8529f33973ac3ecf4940c86ef97ca5a4379c9f
3MF TO STL <dev@3mf2stl.com>
2026-07-28 03:38:08 +0800

    Add Google Analytics (gtag.js) G-6BE5WDTPFT to layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="dark" className="h-full">
+      <head>
+        {/* Google Analytics (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6BE5WDTPFT"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-6BE5WDTPFT');`,
+          }}
+        />
+      </head>
       <body className="min-h-full">{children}</body>
     </html>
   );
