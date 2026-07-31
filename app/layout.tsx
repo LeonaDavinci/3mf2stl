417e240641d17e4a91d496de48e0a1e81d7bc016
LeoDavinci <sixwaters@qq.com>
2026-08-01 03:32:27 +0800

    Remove Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,21 +43,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="dark" className="h-full">
-      <head>
-        {/* Google Analytics (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-6BE5WDTPFT"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-6BE5WDTPFT');`,
-          }}
-        />
-      </head>
       <body className="min-h-full">{children}</body>
     </html>
   );
